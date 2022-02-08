2394229e9c0efa0b863eefa41da5314faa203307
Mathias Bevers <bevertje01@outlook.com>
2022-02-08 16:54:19 +0100

    added jumping

--- GXPEngine/Lavos/Player.cs
+++ GXPEngine/Lavos/Player.cs
@@ -7,9 +7,10 @@ namespace Lavos
 		private const float GRAVITY = 0.3f;
 		private const float JUMP_FORCE = 14.0f;
 		private const float MOVEMENT_SPEED = 3.0f; //TODO: needs testing whether this is to fast.
-		private bool isGrounded = false;
-		private float velocity = 0.0f;
+		private float CurrentLanePosition => game.height - 66.67f - (133.33f * laneNumber) - height;
 
+		private bool isGrounded;
+		private float velocity;
 		private int laneNumber = 1;
 
 		public Player(string fileName, int columns, int rows) : base(fileName, columns, rows)
@@ -18,35 +19,49 @@ namespace Lavos
 			x = game.width * 0.1f;
 		}
 
-		//TODO: 3 lane movement.
-		//TODO: jump.
-
 		/// <summary>
 		///     Update is called by the engine. It handles the player's input.
 		/// </summary>
-		private void Update() { ProcessInput(); }
+		private void Update()
+		{
+			ProcessVerticalInput();
+
+			velocity += GRAVITY;
+
+			if (isGrounded && Input.GetKeyUp(Key.SPACE)) { velocity -= JUMP_FORCE; } //TODO: Smooth the jump.
+
+			isGrounded = false;
+
+			y += velocity;
+
+			if (y >= CurrentLanePosition)
+			{
+				y = CurrentLanePosition;
+				velocity = 0;
+				isGrounded = true;
+			}
+		}
 
 
 		/// <summary>
 		///     Checks if the player has released the "w" or "s", then switches the player's lane.
 		/// </summary>
-		private void ProcessInput()
+		private void ProcessVerticalInput()
 		{
 			if (Input.GetKeyUp(Key.W))
 			{
 				if (laneNumber == 2) { return; }
 
 				++laneNumber;
+				y = CurrentLanePosition;
 			}
 
-			if (Input.GetKeyUp(Key.S))
-			{
-				if (laneNumber == 0) { return; }
+			if (!Input.GetKeyUp(Key.S)) { return; }
 
-				--laneNumber;
-			}
+			if (laneNumber == 0) { return; }
 
-			y = game.height - 66.67f - 133.33f * laneNumber - height;
+			--laneNumber;
+			y = CurrentLanePosition;
 		}
 	}
 }
